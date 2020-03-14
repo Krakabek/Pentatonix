fe3d913c7e75aaaf1ed07ff1c72aedbad1ada3a1
danilradkovsky <dradkovsky@readdle.com>
2020-03-14 19:12:00 +0200

    Website initial

---
+++ docs/precache-manifest.d72c6d6f3edeba88954c5f864f9e562c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e84db68352650d9ce3b09f7c6f8499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1f764dbb34eaf69f4f",
+    "url": "/static/css/main.41a306ed.chunk.css"
+  },
+  {
+    "revision": "7a7e41872d0b121d6736",
+    "url": "/static/js/2.0bc61ea6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0bc61ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1f764dbb34eaf69f4f",
+    "url": "/static/js/main.862a8be4.chunk.js"
+  },
+  {
+    "revision": "f4c6a33f4c09693d6bd2",
+    "url": "/static/js/runtime-main.38cafbe8.js"
+  }
+]);
