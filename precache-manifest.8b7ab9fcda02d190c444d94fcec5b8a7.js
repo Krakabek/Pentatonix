7d91899d06b2639c53345abcf3c1c5f3bb92fc54
danilradkovsky <dradkovsky@readdle.com>
2020-03-14 19:23:40 +0200

    Updates

---
+++ precache-manifest.8b7ab9fcda02d190c444d94fcec5b8a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeca3602443c74479e03331249e7b5f1",
+    "url": "/Pentatonix/index.html"
+  },
+  {
+    "revision": "eef86dd7d74a9abd8fdb",
+    "url": "/Pentatonix/static/css/main.41a306ed.chunk.css"
+  },
+  {
+    "revision": "7a7e41872d0b121d6736",
+    "url": "/Pentatonix/static/js/2.0bc61ea6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Pentatonix/static/js/2.0bc61ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef86dd7d74a9abd8fdb",
+    "url": "/Pentatonix/static/js/main.ef916f74.chunk.js"
+  },
+  {
+    "revision": "6e92587b0fae99e969eb",
+    "url": "/Pentatonix/static/js/runtime-main.f83c0f0c.js"
+  }
+]);
